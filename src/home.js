7b7a52fb6160e78f578ad8782a1fe7d41f75a6fb
Patrick Tweddell <twedds95@hotmail.com>
2022-12-11 17:45:47 -0500

    css updates

--- src/home.js
+++ src/home.js
@@ -8,17 +8,24 @@ export default class Home extends React.Component {
             <div>
                 <h1>Algorithm Viewer Project</h1>
 
-                <p>React app to visualize different algorithms, and other fun projects.</p>
-                <p>This project started off as just a visualization of sorting algorithms, but now takes on any other personal project or challenge I code.</p>
+                <p>Hi! Welcome to my Algorithm Viewer Project.
+                    The goal of this project is not to demonstrate a pretty styled frontend, but rather to visualize how coding concepts that are usually implemented only in the backend work.
+                </p>
+
+                <p>This project was developed using React app to visualize different algorithms.</p>
+
                 <h2>List of Challenges Completed:</h2>
 
                 <h3>Sorting Visualizations</h3>
-                <div>- Merge Sort</div>
-                <div>- Quick Sort</div>
-                <div>- Selection Sort</div>
-                <div>- Insertion Sort</div>
-                <div>- Bead Sort (Gravity Sort)</div>
-
+                <div className="list">
+                    <ul>
+                        <li>Merge Sort</li>
+                        <li>Quick Sort</li>
+                        <li>Selection Sort</li>
+                        <li>Insertion Sort</li>
+                        <li>Bead Sort (Gravity Sort)</li>
+                    </ul>
+                </div>
                 <h3>Path Finding Visualization with A*</h3>
                 <h3>Fourier Transform Visualization</h3>
                 <p>Draw anything and have it drawn back to you using Fourier Transorms to define every continuous line.</p>
